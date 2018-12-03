f08e327a35fceb108b2240f6c0afdf87577e9322
Justin Lowery <j@justin.sx>
2018-12-03 00:04:32 -0500

    rename utils.js to rand-num.js

--- transitions.js
+++ transitions.js
@@ -1,4 +1,4 @@
-const { randNumFromRanges } = require('./utils');
+const { randNumFromRanges } = require('./rand-num');
 
 const isHueSimilar = function (config, lightState, options) {
   return Math.abs(lightState.hue - options.hue) < config.minHueDifference &&
