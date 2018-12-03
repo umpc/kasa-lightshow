0fce5cf5acf57d3547f67ec0a359f6115d9f272d
Justin Lowery <j@justin.sx>
2018-12-02 21:53:58 -0500

    add minHueDifference to increase variation of hues per cycle

--- transitions.js
+++ transitions.js
@@ -1,7 +1,12 @@
 const { randNum } = require('./utils');
 
-const nextLightState = function (config, bulb) {
-  return bulb.lighting.setLightState({
+const isHueSimilar = function (config, lightState, options) {
+  return Math.abs(lightState.hue - options.hue) < config.minHueDifference &&
+    (options.hue !== config.hue[0] || options.hue !== config.hue[1]);
+};
+
+const nextLightState = function (config, bulb, lightState) {
+  const options = {
     on_off: 1,
     color_temp: 0,
     mode: 'normal',
@@ -9,14 +14,21 @@ const nextLightState = function (config, bulb) {
     saturation: randNum(config.saturation[0], config.saturation[1]),
     brightness: randNum(config.brightness[0], config.brightness[1]),
     transition_period: config.nextState.transitionPeriod * 1000
-  });
+  };
+  if (lightState) {
+    if (isHueSimilar(config, lightState, options)) {
+      return nextLightState(config, bulb, lightState);
+    }
+  }
+  bulb.lighting.setLightState(options);
+  return options;
 };
 
 module.exports = {
   startTransitions: function (config, bulb) {
-    nextLightState(config, bulb);
+    let lightState = nextLightState(config, bulb);
     setInterval(
-      function () { nextLightState(config, bulb); },
+      function () { lightState = nextLightState(config, bulb, lightState); },
       config.nextState.interval * 1000
     );
   }
